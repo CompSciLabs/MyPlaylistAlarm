cd4ce5b894b39e528a0e9ccae32ec04c6631e0eb
Matthew Elphick <matt.a.elphy@gmail.com>
2015-07-26 14:19:02 +0100

    Add basic YoutubeService barebones, and call it on MainActivity creation

--- app/src/main/java/io/github/compscilabs/myplaylistalarm/MainActivity.java
+++ app/src/main/java/io/github/compscilabs/myplaylistalarm/MainActivity.java
@@ -1,5 +1,6 @@
 package io.github.compscilabs.myplaylistalarm;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -12,6 +13,8 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = new Intent(this, YoutubeService.class);
+        startService(intent);
     }
 
     @Override
---
+++ app/src/main/java/io/github/compscilabs/myplaylistalarm/YoutubeService.java
@@ -0,0 +1,66 @@
+package io.github.compscilabs.myplaylistalarm;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.IBinder;
+import android.os.Looper;
+import android.os.Message;
+import android.os.Process;
+import android.widget.Toast;
+
+public class YoutubeService extends Service {
+    private Looper mServiceLooper;
+    private ServiceHandler mServiceHandler;
+
+    public YoutubeService() {
+    }
+
+    private final class ServiceHandler extends Handler {
+        public ServiceHandler(Looper looper) {
+            super(looper);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            long endTime = System.currentTimeMillis() + 5*1000;
+            while (System.currentTimeMillis() < endTime) {
+                synchronized (this) {
+                    try {
+                        wait(endTime - System.currentTimeMillis());
+                    } catch (Exception e) {
+                    }
+                }
+            }
+            stopSelf(msg.arg1);
+        }
+    }
+
+    @Override
+    public void onCreate() {
+        HandlerThread thread = new HandlerThread("ServiceStartArguments", Process.THREAD_PRIORITY_BACKGROUND);
+        thread.start();
+        mServiceLooper = thread.getLooper();
+        mServiceHandler = new ServiceHandler(mServiceLooper);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Toast.makeText(this, "Youtube Service Starting", Toast.LENGTH_SHORT).show();
+        Message msg = mServiceHandler.obtainMessage();
+        msg.arg1 = startId;
+        mServiceHandler.sendMessage(msg);
+        return START_STICKY;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onDestroy() {
+        Toast.makeText(this, "Youtube service done", Toast.LENGTH_SHORT).show();
+    }
+}
